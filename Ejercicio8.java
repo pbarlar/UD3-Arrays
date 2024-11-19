416ca8734f2d2bd131c867fd4ae9564e7b1f0890
pbarlar <pbarlar@g.educaand.es>
2024-11-19 10:04:17 +0100

    Update Ejercicio8.java

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,5 +1,9 @@
 public class Ejercicio8 {
     public static void main (String[] args){
-        
+        int numeros[]=new int[100];
+        for (int i=0;i<100;i++){
+            numeros[i]=(int)(Math.random()*20);
+            System.out.print(numeros[i]+" ");
+        }
     }
 }
