7231a265da0635b105fd43163e52e038af09c67d
pbarlar <pbarlar@g.educaand.es>
2024-11-19 09:39:30 +0100

    Update Ejercicio7.java

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,6 +1,8 @@
 public class Ejercicio7 {
     public static void main (String[] args){
+        int numeros[]=new int[20];
         for(int i=0;i<20;i++){
-            
+            numeros[i]=(int)Math.random()*100;
         }
+        
 }
