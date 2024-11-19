b52050abc0f43d8291e20e03878cd4f409c14b4c
pbarlar <pbarlar@g.educaand.es>
2024-11-19 10:31:00 +0100

    update

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Ejercicio8 {
     public static void main (String[] args){
+        Scanner entrada=new Scanner(System.in);
         int numeros[]=new int[100];
         for (int i=0;i<100;i++){
             numeros[i]=(int)(Math.random()*20);
             System.out.print(numeros[i]+" ");
         }
+        System.out.println();
+        System.out.println("Indica un valor que quieres introducir en el array: ");
+        int numeroentra=entrada.nextInt();
+        System.out.println("Indica el valor que quieres quitar del array: ");
+        int numerosale=entrada.nextInt();
+        for(int i=0;i<100;i++){
+            if (numeros[i]==numerosale) {
+                numeros[i]=numeroentra;
+            }
+        }
+        System.out.println("Cadena cambiada: ");
+        for (int i = 0; i < 100; i++) {
+            System.out.print(numeros[i]+" ");
+        }
+        
     }
 }
---
+++ Ejercicio9.java
@@ -0,0 +1,23 @@
+public class Ejercicio9 {
+    public static void main (String[] args){
+        int i=0;
+        int j=0;
+        int numeros[]=new int[20];
+        for(i=0;i<20;i++){
+            numeros[i]=(int)(Math.random()*100);
+            System.out.print(numeros[i]+" ");
+            if (numeros[i]%2==0) {
+                for (j = 0; j < numeros.length; j++) {
+                    int numj=numeros[j];
+                    int numi=numeros[i];
+                    numeros[j]=numeros[i];
+                    numeros[i]=numj;
+                }
+            }
+        }
+        System.out.println();
+        for(i=0;i<20;i++){
+            System.out.print(numeros[i]+" ");
+        }
+    }
+}
